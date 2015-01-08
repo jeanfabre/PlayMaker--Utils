c6ccdc8922bcb71b43c240df62fded316f890561
jeanfabre <contact@fabrejean.net>
2015-01-08 11:23:22 +0300

    initial work to list events

--- PlayMakerUtils_Events.cs
+++ PlayMakerUtils_Events.cs
@@ -13,7 +13,38 @@ using UnityEngine;
 using HutongGames.PlayMaker;
 
 public partial class PlayMakerUtils {
-	
+
+	/// <summary>
+	/// Initial work to create some menus for editors
+	/// </summary>
+	/// <param name="fromFsm">From fsm.</param>
+	public static void GetFsmEvents(PlayMakerFSM fromFsm)
+	{
+		if (fromFsm==null)
+		{
+			return;
+		}
+
+		Debug.Log("fsm events ( found in the events tag, not necessarly used, warning");
+		foreach(var _event in fromFsm.FsmEvents)
+		{
+			Debug.Log(_event.Name +", is global: "+_event.IsGlobal);
+		}
+
+		Debug.Log("global transitions events, actually implemented in that fsm");
+		foreach(var _globaltransition in fromFsm.FsmGlobalTransitions)
+		{
+			var _event = _globaltransition.FsmEvent;
+			Debug.Log(_event.Name +", is global: "+_event.IsGlobal);
+		}
+
+		Debug.Log("global events, within this project");
+		foreach(var name in PlayMakerGlobals.Instance.Events)
+		{
+			Debug.Log(name);
+		}
+
+	}
 
 	public static void SendEventToGameObject(PlayMakerFSM fromFsm,GameObject target,string fsmEvent)
 	{
