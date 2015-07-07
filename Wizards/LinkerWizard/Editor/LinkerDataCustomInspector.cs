64230be0fba34ad70b3ac72a13b99d271abfef84
jeanfabre <contact@fabrejean.net>
2015-07-07 17:02:38 +0300

    Linker Wizard scriptableObject draft

---
+++ Wizards/LinkerWizard/Editor/LinkerDataCustomInspector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+using HutongGames.PlayMakerEditor;
+
+using HutongGames.PlayMaker.Ecosystem.Utils;
+
+[CustomEditor(typeof(LinkerData))]
+public class LinkerDataCustomInspector : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		LinkerData _target = target as LinkerData;
+
+		FsmEditorStyles.Init();
+
+		//GUILayout.Box("Hello",FsmEditorStyles.LargeTitleWithLogo,GUILayout.Height(42f));
+		GUI.Box (new Rect (0f, 0f, Screen.width, 42f), "Linker Wizard", FsmEditorStyles.LargeTitleWithLogo);
+
+		GUILayout.Label("Check 'debug' for tracking all reflections");
+
+		bool _debug = EditorGUILayout.Toggle("Debug",_target.debug);
+		if (_debug!=_target.debug)
+		{
+			_target.debug = _debug;
+			EditorUtility.SetDirty(_target);
+		}
+
+
+	}
+
+}
---
+++ Wizards/LinkerWizard/LinkerData.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+
+using UnityEngine;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace HutongGames.PlayMaker.Ecosystem.Utils
+{
+	public class LinkerData : ScriptableObject
+	{
+		public bool debug;
+
+		static public bool DebugAll
+		{
+			get{
+				if (instance==null)
+				{
+					return true;
+				}
+				return instance.debug;
+			}
+		}
+
+
+		static public LinkerData instance;
+		
+
+		void OnEnable() {
+			LinkerData.instance = this;
+		}
+
+
+	#if UNITY_EDITOR
+
+		[MenuItem("PlayMaker/Addons/Tools/Linker Wizard")]
+		[MenuItem("Assets/Create/PlayMaker/Linker Wizard")]
+		public static void CreateAsset ()
+		{
+			if (LinkerData.instance!=null)
+			{
+				Selection.activeObject = LinkerData.instance;
+				EditorGUIUtility.PingObject(Selection.activeObject);
+				Debug.Log("Linker Wizard already exists");
+				return;
+			}
+			PlayMakerUtils.CreateAsset<LinkerData>();
+		}
+
+	#endif
+
+	}
+}
